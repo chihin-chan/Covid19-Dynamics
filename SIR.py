24cc8d03775cffc0cf17688a6ee3fafe400c89d9
chanbuntu <chanchihin_1@hotmail.com>
2020-04-07 17:57:07 +0100

    Working Optimisation for Beta/Gamma

--- SIR.py
+++ SIR.py
@@ -12,54 +12,83 @@ import scipy as sp
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
-from scipy.optimize import least_squares
+from scipy.optimize import minimize
 
-# ODE Function
-def sir(t,y):
+# Predict function
+def predict(x, infect_data, recovered_data, init, beta, gamma):
     
-    gamma = 0.013309
-    beta =  0.00001774
+    # ODE Function
+    def sir(t, y):
+            
+        # Outputs
+        S = y[0]
+        I = y[1]
+        R = y[2]
+        
+        return [-beta*I*S, beta*I*S - gamma*I, gamma*I]
     
-    S = y[0]
-    I = y[1]
-    R = y[2]
-    return [-beta*I*S, beta*I*S - gamma*I, gamma*I]
+    t_extend = len(x) + 100
+    t = np.linspace(0,t_extend, t_extend)
+    res = solve_ivp(sir, 
+                    [0, t_extend], 
+                    [init[0], init[1], init[2]],
+                    t_eval=np.arange(0,t_extend,1))
+    
+    plt.plot(t, res.y[0])
+    plt.plot(t, res.y[1])
+    plt.plot(t, res.y[2])
+    plt.plot(x, infect_data)
+    plt.plot(x, recovered_data)
+    plt.legend(("S","I","R", "I_exact", "R_exact"))
+    plt.grid()
+    plt.show()
+    
 
 # Cost function
-def cost(x, t, init):
-    t_eval = np.arange(0,t,1)
-    res = solve_ivp(sir, [0, t], [init[0], init[1], init[2]], t_eval=np.arange(0,t,1))
+def cost(point, infect_data, recovered_data, init):
     
+    beta, gamma = point
+    
+    # ODE Function
+    def sir(t, y):
+            
+        # Outputs
+        S = y[0]
+        I = y[1]
+        R = y[2]
+        
+        return [-beta*I*S, beta*I*S - gamma*I, gamma*I]
+    
+    res = solve_ivp(sir, 
+                    [0, len(infect_data)], 
+                    [init[0], init[1], init[2]],
+                    t_eval=np.arange(0,len(infect_data),1))
     # Costs/errors
-    infect_error = np.zeros(len(x))
-    for i in range(0,len(x)):
-        infect_error[i] = res.y[1][i] - x[i]
-
-
-    return infect_error
-
-    
-
+    # Returns RMSE
+    rmse_infect = np.sqrt(np.mean((res.y[1] - infect_data)**2)) 
+    rmse_recoverd = np.sqrt(np.mean((res.y[2] - recovered_data)**2)) 
+    alpha = 0.1
+    print(alpha*rmse_infect + (1-alpha)*rmse_recoverd)
+    return alpha*rmse_infect + (1-alpha)*rmse_recoverd
 
 c_df = pd.read_csv('time_series_covid19_confirmed_global.csv')
 r_df = pd.read_csv('time_series_covid19_recovered_global.csv')
 
-country = "Switzerland"
-start_date = "2/26/20"
-start_date_2 = "2/26/2020"
+country = "Ireland"
+start_date = "1/23/20"
 
 # Parsing confirmed/recovered cases from csv
 confirmed = c_df[c_df['Country/Region'] == country].iloc[0].loc[start_date:]
-recovered = r_df[r_df['Country/Region'] == country].iloc[0].loc[start_date_2:]
+recovered = r_df[r_df['Country/Region'] == country].iloc[0].loc[start_date:]
 # Truncate last element to fit recovered data
-confirmed = confirmed.values[0:-1]  
+confirmed = confirmed.values 
 recovered = recovered.values
 
 # I(t)
 infect = confirmed - recovered
 
-# Suceptable = Total Population
-N = 20000
+# Suceptable
+N = 10000
 
 # Number of infected at t0
 I0 = 2
@@ -70,15 +99,15 @@ R0 = 0
 S = N - confirmed
 
 # Parameters to optimise
-res_lsq = least_squares(cost(infect, len(infect), [S0, I0, R0]), 
+optimal = minimize(cost, 
+                   [0.00001, 0.00001], 
+                   args=(infect, recovered, [S0, I0, R0]),
+                   method='L-BFGS-B',
+                   bounds=[(0.00000001, 0.4), (0.00000001, 0.4)])
+print(optimal.x)
 
+t = np.linspace(0,len(infect), len(infect))
 
-plt.plot(res.y[0])
-plt.plot(res.y[1])
-plt.plot(res.y[2])
-plt.plot(infect)
-plt.plot(recovered)
-plt.legend(('S', 'I', 'R', 'Actual Infected', 'Actual Recovered'))
-plt.grid()
-plt.show()
+predict(np.transpose(t), infect, recovered, [S0, I0, R0], optimal.x[0], optimal.x[1])
+
 
